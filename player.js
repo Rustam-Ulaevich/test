f2fb8abde38492c8f5a4669cd97915b29e94948e
Rustam-Ulaevich <praporglamur@gmail.com>
2024-02-06 13:44:19 +0500

    added mp3 files

---
+++ player.js
@@ -0,0 +1,22 @@
+let playlist = {
+  title: "Hip-Hop Hits",
+  coverImage: "./hip-hop-hits.jpg",
+  info: {
+    totaltracksCount: 4,
+    totalTracksDuration: 733,
+  },
+  tracks: [
+    {
+      artist: "Eminem",
+      title: "Rap God",
+      fileUrl: "tracksEminem_-_Rap_God.mp3",
+      isHot: false,
+    },
+    {
+      artist: "50cent",
+      title: "In da club",
+      fileUrl: "tracks\50_Cent_-_In_Da_Club.mp3",
+      isHot: false,
+    },
+  ],
+};
--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const h1el = document.createElement("h1");
+// data
+const incubatorInfo = {
+  title: "it-incubator",
+  graduatesCount: 2000,
+  areYouChampion: true,
+  tecnologies: [{ title: "Front" }, { title: "Back" }, { title: "DevOps" }],
+};
 
-document.body.append(h1el);
+//render
+let pageTitleElement = document.createElement("h1");
+pageTitleElement.innerText = incubatorInfo.title;
+document.body.append(pageTitleElement);
+
+let graduatesCountElement = document.createElement("input");
+graduatesCountElement.value = incubatorInfo.graduatesCount;
+graduatesCountElement.type = "number";
+document.body.append(graduatesCountElement);
+
+let areYouChampionElement = document.createElement("input");
+areYouChampionElement.type = "checkbox";
+areYouChampionElement.checked = incubatorInfo.areYouChampion;
+document.body.append(areYouChampionElement);
+
+let techSelectElement = document.createElement("s");
